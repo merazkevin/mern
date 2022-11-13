894d1a1e72c2e468789c759ecba418ebce88022c
merazkevin <merazkevin@yahoo.com>
2022-11-13 09:52:06 -0800

    nul

--- projectManager/server/controllers/Product.controller.js
+++
@@ -1,5 +0,0 @@
-const ProductController = require('../Product.controller');
-
-module.exports = function (app) {
-    app.get('/api', ProductController.index);
-}
--- projectManager/server/models/Product.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const ProductSchema = new mongoose.Schema({
-    title: { type: String },
-    price: { type: String },
-    description: { type: String }
-}, { timestamps: true });
-module.exports.Person = mongoose.model('Person', PersonSchema);
